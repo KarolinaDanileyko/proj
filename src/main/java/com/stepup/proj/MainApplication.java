8228a3efc4f8eecc306a2efc50c0bf29e3543b9e
Karolina Danileyko <KDanileyko@inno.tech>
2024-02-07 22:55:11 +0300

    Добавлено к основной логике

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -6,5 +6,7 @@ public class MainApplication {
         System.out.println("111111111111");
         System.out.println("222222222222");
         System.out.println("333333333333");
+        System.out.println("444444444444");
+        System.out.println("555555555555");
     }
 }
