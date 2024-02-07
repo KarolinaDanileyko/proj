7901618516ae25c49647b742379fc2f9c5d52637
Karolina Danileyko <KDanileyko@inno.tech>
2024-02-07 22:46:52 +0300

    Изменена основная логика Добавлен класс коробки

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -4,5 +4,7 @@ public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("111111111111");
+        System.out.println("222222222222");
+        System.out.println("333333333333");
     }
 }
