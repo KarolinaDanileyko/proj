2adb2facb0a14767353d5753527863363624a855
Karolina Danileyko <KDanileyko@inno.tech>
2024-02-07 23:11:32 +0300

    Улучшили логику вывода сообщений

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,10 +3,10 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("111111111111");
-        System.out.println("222222222222");
-        System.out.println("333333333333");
-        System.out.println("444444444444");
-        System.out.println("555555555555");
+        System.out.println("1----это------1");
+        System.out.println("2----так------2");
+        System.out.println("3----сделали--3");
+        System.out.println("4----задачу---4");
+        System.out.println("5----3605-----5");
     }
 }
